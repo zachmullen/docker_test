0e71123cda57859ea82031002d410257f15c0ae3
Zach Mullen <zach.mullen@kitware.com>
2016-06-08 11:44:43 -0400

    Add a stream test endpoint

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,10 @@
+import datetime
+
 from girder.api import access
 from girder.api.describe import Description, describeRoute
-from girder.api.rest import Resource, loadmodel, filtermodel
+from girder.api.rest import Resource, loadmodel, filtermodel, iterBody, getApiUrl
 from girder.constants import AccessType
+from girder.models.notification import ProgressState
 from girder.plugins.worker import utils
 
 
@@ -9,46 +12,54 @@ class DockerTest(Resource):
     def __init__(self):
         Resource.__init__(self)
 
-        self.route('POST', ('outputs',), self.testOutputs)
+        #self.route('POST', ('outputs',), self.testOutputs)
+        self.route('POST', ('stream',), self.testStream)
+        self.route('POST', ('stream_callback',), self.streamUpload)
 
     @access.user
+    @loadmodel(map={'folderId': 'folder'}, model='folder', level=AccessType.WRITE)
+    @loadmodel(map={'itemId': 'item'}, model='item', level=AccessType.READ)
     @filtermodel('job', 'jobs')
     @describeRoute(
         Description('Test docker outputs.')
+        .param('folderId', 'Where to write container outputs.')
+        .param('itemId', 'The input file.')
     )
-    def testOutputs(self, params):
+    def testOutputs(self, folder, item, params):
         token = self.getCurrentToken()
 
         jobModel = self.model('job', 'jobs')
 
         job = jobModel.createJob(
-            title='docker output stream test', type='docker_test',
+            title='docker output test: %s' % folder['name'], type='docker_test',
             handler='worker_handler', user=self.getCurrentUser())
         jobToken = jobModel.createJobToken(job)
 
         kwargs = {
             'task': {
                 'mode': 'docker',
-                'docker_image': 'test/test:latest',
+                'docker_image': 'testoutputs:latest',
                 'pull_image': False,
-                'inputs': [],
-                'outputs': [{
-                    'id': 'my_named_pipe',
+                'inputs': [{
+                    'id': 'input',
                     'target': 'filepath',
                     'format': 'text',
-                    'stream': True,
+                    'type': 'string'
+                }],
+                'outputs': [{
+                    'id': 'out.txt',
+                    'target': 'filepath',
+                    'format': 'text',
                     'type': 'string'
                 }]
             },
-            'inputs': {},
+            'inputs': {
+                'input': utils.girderInputSpec(
+                    item, resourceType='item', token=token)
+            },
             'outputs': {
-                'my_named_pipe': {
-                    'mode': 'http',
-                    'url': 'http://localhost:8801',
-                    'method': 'POST',
-                    'format': 'text',
-                    'type': 'string',
-                }
+                'out.txt': utils.girderOutputSpec(
+                    folder, token)
             },
             'jobInfo': utils.jobInfoSpec(job, jobToken)
         }
@@ -56,6 +67,62 @@ class DockerTest(Resource):
         job = jobModel.save(job)
         jobModel.scheduleJob(job)
         return job
+
+    @access.user
+    @filtermodel('job', 'jobs')
+    @describeRoute(
+        Description('Test docker streaming output.')
+    )
+    def testStream(self, params):
+        token = self.getCurrentToken()
+
+        jobModel = self.model('job', 'jobs')
+
+        job = jobModel.createJob(
+            title='docker stream test', type='docker_test',
+            handler='worker_handler', user=self.getCurrentUser())
+        jobToken = jobModel.createJobToken(job)
+
+        kwargs = {
+            'task': {
+                'mode': 'docker',
+                'docker_image': 'testoutputs:latest',
+                'pull_image': False,
+                'inputs': [],
+                'outputs': [{
+                    'id': 'my_named_pipe',
+                    'target': 'filepath',
+                    'stream': True
+                }]
+            },
+            'inputs': {},
+            'outputs': {
+                'my_named_pipe': {
+                    'mode': 'http',
+                    'method': 'POST',
+                    'url': getApiUrl() + '/docker_test/stream_callback'
+                }
+            },
+            'validate': False,
+            'auto_convert': False,
+            'jobInfo': utils.jobInfoSpec(job, jobToken)
+        }
+        job['kwargs'] = kwargs
+        job = jobModel.save(job)
+        jobModel.scheduleJob(job)
+        return job
+
+    @access.user
+    @describeRoute(None)
+    def streamUpload(self, params):
+        nmodel = self.model('notification')
+        note = nmodel.initProgress(self.getCurrentUser(), 'Docker stream demo')
+        for i, chunk in enumerate(iterBody(), 1):
+            nmodel.updateProgress(note, message='%d: %s' % (i, chunk))
+
+        nmodel.updateProgress(
+            note, state=ProgressState.SUCCESS,
+            expires=datetime.datetime.utcnow() + datetime.timedelta(seconds=10))
 
 
 def load(info):
